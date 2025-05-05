cc36688390043a16f2750eec58713afff81ae682
Clein2312 <cleinsarmiento@hotmail.com>
2025-05-06 00:16:05 +0200

    Updated Content view
    Added comments for _test AddItem and deleteItems

--- ToDo_Vapor/ContentView.swift
+++ ToDo_Vapor/ContentView.swift
@@ -12,7 +12,7 @@ struct ContentView: View {
     @Environment(\.modelContext) private var modelContext
     @Query private var items: [ToDoItem]
     @State private var newItemTitle: String = ""
-    @StateObject var viewModel: ToDoItemViewModel
+    @State var viewModel: ToDoItemViewModel
     @State private var isLoading: Bool = false
     @State private var selectedItem: ToDoItem?
     
@@ -28,13 +28,15 @@ struct ContentView: View {
                     HStack {
                         TextField("New ToDO item...", text: $newItemTitle)
                             .onSubmit {
-                                addItem()
+                                //addItem()
+                                addItem_test()
                             }
                             .padding()
                             .cornerRadius(8)
                             .font(.headline)
                         
-                        Button(action: addItem){
+                        //Button(action: addItem){
+                        Button(action: addItem_test){
                             Image(systemName: "plus").font(.title2)
                         }.disabled(newItemTitle.isEmpty)
                     }.padding(.horizontal)
@@ -54,7 +56,8 @@ struct ContentView: View {
                                     ToDoItemRow(item: item)
                                 }
                             }
-                            .   onDelete(perform: deleteItems)
+                            //.onDelete(perform: deleteItems)
+                            .onDelete(perform: deleteItems_test)
                         }
                     }
                 }
@@ -71,11 +74,27 @@ struct ContentView: View {
             } detail: {
                 Text("Select an item")
             }
+            .task{
+                setupViewModel()
+            }
         }
         
+//MARK: API Synchronizatopm
+    private func setupViewModel() {
+        viewModel.onAppear(modelContext: modelContext)
+        isLoading = true
+        Task {
+            await viewModel.fetchToDoItems()
+            await MainActor.run {
+                isLoading = false
+            }
+        }
+    }
+    
             
-
-    private func addItem() {
+//MARK: Add an Item:
+    
+    private func addItem_test() {
         
         withAnimation {
             let newItem = ToDoItem(timestamp: Date(), title: newItemTitle)
@@ -83,14 +102,50 @@ struct ContentView: View {
             newItemTitle = ""
         }
     }
+    
+    private func addItem(){
+        guard !newItemTitle.isEmpty else { return }
+        
+        withAnimation {
+            let data = ToDoItemCreateDTO(
+                title: newItemTitle
+            )
+            
+            Task {
+                isLoading = true
+                await viewModel.createToDoItem(data: data)
+                newItemTitle = ""
+                isLoading = false
+            }
+        }
+    }
 
-    private func deleteItems(offsets: IndexSet) {
+    //Mark Delete Items
+    private func deleteItems_test(offsets: IndexSet) {
         withAnimation {
             for index in offsets {
                 modelContext.delete(items[index])
             }
         }
     }
+    
+    private func deleteItems(offsets: IndexSet){
+        withAnimation {
+            for index in offsets {
+                deleteItem(items[index])
+            }
+        }
+    }
+    
+    private func deleteItem(_ item: ToDoItem){
+        Task {
+            isLoading = true
+            await viewModel.deleteToDoItem(toDoItem: item)
+            isLoading = false
+        }
+    }
+    
+    
     
     private func printItemInfo(item: ToDoItem)-> some View{
         VStack(alignment: .leading){
@@ -115,6 +170,7 @@ struct ContentView: View {
 }
 
 #Preview {
-    ContentView( viewModel: ToDoItemViewModel())
+    var myViewModel = ToDoItemViewModel()
+    ContentView( viewModel: myViewModel)
         .modelContainer(for: ToDoItem.self, inMemory: true)
 }
